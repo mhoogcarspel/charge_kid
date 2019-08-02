3d0c935872dce9dba1d6a71e265b52c6d6182e90
Ayrton <makkun1232@gmail.com>
2019-08-02 19:45:10 -0300

    ADD jump and move functions to players

--- actors/Player.gd
+++ actors/Player.gd
@@ -3,21 +3,42 @@ extends KinematicBody2D
 export(float) var gravity_acceleration
 export(float) var horizontal_acceleration
 export(float) var horizontal_max_speed
-
 export(float) var number_of_jumps
+export(float) var coyote_jump_timer
+export(float) var jump_velocity
 
 onready var can_jump: bool = true
 
+var move: float
+var velocity := Vector2()
+
 func _physics_process(delta):
+	
 	if self.is_on_floor() && number_of_jumps > 0:
 		can_jump = true #Add coyote jump timer
-		pass
+	if !self.is_on_floor():
+		$CoyoteTimer.start()
+	
+	move(get_directional_inputs(), delta)
+	jump()
 	
 
-func get_diretional_inputs() -> Vector2:
+
+func move(direction: Vector2, delta: float) -> void:
+	if direction.x != 0:
+		velocity.x += direction.x*horizontal_acceleration*delta
+
+func jump() -> void:
+	if can_jump:
+		velocity.y -= jump_velocity
+
+func get_directional_inputs() -> Vector2:
 	var directionals = Vector2(
 	Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	, Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right"))
 	
 	directionals.normalized()
 	return directionals
+
+func _on_CoyoteTimer_timeout() -> void:
+	can_jump = false
