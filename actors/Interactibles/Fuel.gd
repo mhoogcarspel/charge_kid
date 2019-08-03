86cdf3633d9477a8295e562a530ee8af57f2ca83
Rafael <rafaelbordoni00@gmail.com>
2019-08-03 13:28:18 -0300

    Add more functions to the fuel tank

--- actors/Interactibles/Fuel.gd
+++ actors/Interactibles/Fuel.gd
@@ -1,7 +1,52 @@
 extends StaticBody2D
 
+
+
+export (String, "Full", "Empty") var state setget change_state
+
+var has_fuel: bool
+
+
+
+func change_state(new_state: String) -> void:
+	if new_state == "Full":
+		has_fuel = true
+		$FuelTank.frame = 6
+		state = new_state
+	elif new_state == "Empty":
+		has_fuel = false
+		$FuelTank.frame = 0
+		state = new_state
+
+
+
+func is_full() -> bool:
+	return has_fuel
+
+func is_empty() -> bool:
+	return not has_fuel
+
+
+
+func fill() -> void:
+	if is_empty():
+		$FuelTank/AnimationPlayer.play("Filling")
+		has_fuel = true
+
+func empty() -> void:
+	if is_full():
+		$FuelTank/AnimationPlayer.play("Emptying")
+		has_fuel = false
+
 func hit(bullet: PhysicsBody2D):
-	bullet.charge_bullet()
-	$FuelTank/AnimationPlayer.play("Hit")
-	yield($FuelTank/AnimationPlayer,"animation_finished")
-	$FuelTank/AnimationPlayer.play("Emptying")
+	if is_full():
+		bullet.charge_bullet()
+		$FuelTank/AnimationPlayer.play("Hit")
+		has_fuel = false
+
+
+
+
+
+
+
