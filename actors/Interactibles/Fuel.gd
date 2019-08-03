a4aa9a8e1883bf90e3cb4c27fcc6cbea44559241
Ayrton <makkun1232@gmail.com>
2019-08-03 13:09:02 -0300

    ADD Fuel to testing level

--- actors/Interactibles/Fuel.gd
+++ actors/Interactibles/Fuel.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
 func hit(bullet: PhysicsBody2D):
-	$FuelTank/AnimationPlayer.play("Hit")
 	bullet.charge_bullet()
+	$FuelTank/AnimationPlayer.play("Hit")
+	yield($FuelTank/AnimationPlayer,"animation_finished")
+	$FuelTank/AnimationPlayer.play("Emptying")
