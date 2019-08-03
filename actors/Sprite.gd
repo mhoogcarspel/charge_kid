42a2fd9d563d1f7cc82fa6c99d7851cba6518029
Ayrton <makkun1232@gmail.com>
2019-08-02 21:31:03 -0300

    ADD flip function on player sprite

---
+++ actors/Sprite.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+onready var player:KinematicBody2D = self.get_parent()
+
+func _process(delta):
+	flip_sprite(player.facing)
+	pass
+
+func flip_sprite(facing) -> void:
+	if facing > 0 && flip_h:
+		flip_h = false
+	elif facing < 0 && !flip_h:
+		flip_h = true
