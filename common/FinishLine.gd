d214c4244a7aac18e20e84b0db0fd31226a9a588
Ayrton <makkun1232@gmail.com>
2019-08-04 10:09:07 -0300

    ADD win condition

---
+++ common/FinishLine.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+onready var main = get_tree().get_nodes_in_group("main")
+
+export(PackedScene) var next_stage
+
+func _on_FinishLine_body_entered(body):
+	if body.is_in_group("player"):
+		main.change_scene(next_stage)
