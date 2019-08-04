3e1535c061aadae0e1b025317c6de3d1e283520a
Ayrton <makkun1232@gmail.com>
2019-08-04 04:23:02 -0300

    ADD Start Menu

---
+++ Menu/StartMenu.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_StartGame_pressed():
+	print("Start Game")
+
+
+func _on_Credits_pressed():
+	print("Credits")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
