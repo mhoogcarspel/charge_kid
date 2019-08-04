a07bf0f3256cefa28876ea51cea5579f472776b1
Rafael <rafaelbordoni00@gmail.com>
2019-08-04 09:23:31 -0300

    End of first level

--- levels/Area2D3.gd
+++ levels/Area2D3.gd
@@ -2,13 +2,10 @@ extends Area2D
 
 
 
+
 func _process(_delta):
 	for body in get_overlapping_bodies():
 		if body.is_in_group("player"):
-			if body.can_shoot and $Timer.is_stopped():
-				body.write("X: shoot")
+			if body.can_boost and $Timer.is_stopped():
+				body.write("C: boost")
 				$Timer.start()
-
-
-
-
