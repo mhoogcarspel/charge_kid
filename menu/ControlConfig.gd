6d8d1f54190bfbdc59f2f6e59415a4982342135c
Ayrton Makoto <makkun1232@gmail.com>
2019-08-07 15:48:01 -0300

    ADD Controls configuration screen

---
+++ menu/ControlConfig.gd
@@ -0,0 +1,39 @@
+extends Control
+
+export var next_scene:PackedScene
+
+onready var actions: Dictionary = {
+	"ui_jump": "Jump",
+	"ui_shoot": "Shoot",
+	"ui_boost": "Boost",
+	"ui_left": "Left",
+	"ui_right": "Right",
+	"ui_up": "Up",
+	"ui_down": "Down"
+	}
+onready var control_handler = ButtonGetter.new(actions.keys())
+onready var controls_iterator: int = 0
+onready var configuring: bool = false
+onready var key_pressed:InputEventKey = null
+
+func _ready():
+	$VBoxContainer/HBoxContainer/MessageControls.text = " Time to decide your controls. \n We recommend zxc + arrows or equivalent. \n Press any key when you're ready."
+
+func _input(event):
+	if event is InputEventKey and just_pressed(event):
+		if configuring:
+			if !control_handler.find_another_action_with_same_key(actions.keys()[controls_iterator], event):
+				$VBoxContainer/Error.text = ""
+				control_handler.change_key_binding(actions.keys()[controls_iterator], event)
+				controls_iterator += 1
+				if controls_iterator > actions.size():
+					pass
+			else:
+				$VBoxContainer/Error.text = "\nERROR: You already assigned that key to another action,\n try another key."
+		else:
+			configuring = true
+		
+		$VBoxContainer/HBoxContainer/MessageControls.text = "Press a key to " + actions[actions.keys()[controls_iterator]]
+
+func just_pressed(event: InputEvent) -> bool:
+	return event.is_pressed() and !event.is_echo()
