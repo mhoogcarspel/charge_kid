c8fd3c42a0788819febbfb115517c95d95163c6f
Ayrton <makkun1232@gmail.com>
2019-08-15 09:06:42 -0300

    Tweaks

--- menu/config_controls_menu/ChangeKeyDialog.gd
+++ menu/config_controls_menu/ChangeKeyDialog.gd
@@ -15,11 +15,14 @@ func parse(action: String, control_handler: ButtonGetter):
 
 func _input(event):
 	if control_handler.is_keyboard_or_gamepad_key(event) and control_handler.just_pressed(event) and !configured:
-		control_handler.find_and_erase_another_action_with_same_key(action, event)
-# warning-ignore:return_value_discarded
-		control_handler.change_key_binding(action, event)
-		configured = true
-		#self.exit()
+		if !control_handler.find_another_action_with_same_key(action, event):
+			control_handler.change_key_binding(action, event)
+			configured = true
+		else:
+			$LabelBaseModel.text = error_message()
+
+func error_message() -> String:
+	return ("ERROR: button already mapped,\n try another button")
 
 func _process(delta):
 	if configured and !Input.is_action_pressed(action):
