43035852205da530da6e0b769ce353355b3b0faf
Rafael <rafaelbordoni00@gmail.com>
2019-08-19 14:47:28 -0300

    FIX background parallax bug

--- assets/Tilemaps/Parallax.gd
+++ assets/Tilemaps/Parallax.gd
@@ -6,17 +6,15 @@ export (float) var ratio = 2
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var camera: Camera2D = player.get_node("PlayerCamera")
-onready var on: bool = false
 
 
-func _ready():
-	var timer = Timer.new()
-	timer.start(0.1)
-	yield(timer, "timeout")
-	on = true
 
 func _process(delta):
-	if on:
+	player = get_tree().get_nodes_in_group("player")[0]
+	if player != null:
+		camera = player.get_node("PlayerCamera")
+	if camera != null:
 		position.x = camera.get_camera_screen_center().x/ratio - 128
+
 
 
