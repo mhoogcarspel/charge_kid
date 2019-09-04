0639c3e1d949bbc7cf86f778852d2f2a8916a92c
Unknown <rafaelbordoni00@gmail.com>
2019-09-04 17:23:55 -0300

    ADD new 3rd level

---
+++ levels/Messages/Lv3SpMessage.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+
+export (String) var message
+export (float) var message_time = 2.0
+onready var player_detected: bool = false
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("player"):
+		player_detected = true
+
+func _on_Lv3SpMessage_body_entered(body):
+	if body.is_in_group("player") and not player_detected:
+		player_detected = true
+		body.write(message, message_time/2)
