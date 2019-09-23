c4cbe319ba3139eef5c07cbd38de7b5b9936a636
Unknown <rafaelbordoni00@gmail.com>
2019-09-23 12:52:30 -0300

    Add screen transitions between levels

---
+++ levels/ScreenTransition.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+func _ready():
+	get_tree().paused = true
+	$ColorRect.visible = true
+	$CenterContainer/Label.visible = true
+
+func _input(event):
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion or event is InputEventKey and $PityTimer.is_stopped() and not event.is_echo():
+		_on_Timer_timeout()
+
+func _on_Timer_timeout():
+	get_tree().paused = false
+	self.queue_free()
+
+
+
