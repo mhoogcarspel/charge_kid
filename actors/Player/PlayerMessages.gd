7311d8a76f4f15f0f96c8deddc855e1859e68e40
Rafael <rafaelbordoni00@gmail.com>
2019-09-30 18:08:41 -0300

    Finish 5th level

--- actors/Player/PlayerMessages.gd
+++ actors/Player/PlayerMessages.gd
@@ -4,8 +4,15 @@ extends Label
 
 onready var player = get_parent()
 
-func _process(_delta):
+func _physics_process(_delta):
 	if player.position.y <= 36:
 		rect_position.y = 36
 	else:
 		rect_position.y = -36
+	
+	if player.position.x < rect_size.x/2:
+		rect_position.x = -player.position.x
+	elif player.position.x > player.level_length - rect_size.x/2:
+		rect_position.x = -(player.level_length - player.position.x)
+	else:
+		rect_position.x = -rect_size.x/2
--- levels/Messages/Message.gd
+++ levels/Messages/Message.gd
@@ -4,5 +4,6 @@ export (String) var message
 export (float) var message_time = 2.0
 
 func _on_Area2D4_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and $Timer.is_stopped():
 		body.write(message, message_time/2)
+		$Timer.start()
