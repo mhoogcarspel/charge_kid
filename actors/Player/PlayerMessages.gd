131789680505ff2d9be8b14e09af2db13502e632
Rafael <rafaelbordoni00@gmail.com>
2019-10-11 15:29:16 -0300

    Update player messages

--- actors/Player/PlayerMessages.gd
+++ actors/Player/PlayerMessages.gd
@@ -13,6 +13,6 @@ func _physics_process(_delta):
 	if player.position.x < rect_size.x/2:
 		rect_position.x = -player.position.x
 	elif player.position.x > player.level_length - rect_size.x/2:
-		rect_position.x = -(player.level_length - player.position.x)
+		rect_position.x = (player.level_length - player.position.x) - rect_size.x
 	else:
 		rect_position.x = -rect_size.x/2
