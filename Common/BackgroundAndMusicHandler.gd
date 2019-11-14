95efa3ddf953eecaa67f5a46728019a908c72b98
vicentereis <vicentersfa@gmail.com>
2019-11-13 21:17:54 -0300

    O primeiro de muitos

--- Common/BackgroundAndMusicHandler.gd
+++ Common/BackgroundAndMusicHandler.gd
@@ -1,24 +1,53 @@
 extends AudioStreamPlayer
 
+onready var mus = AudioServer.get_bus_index("MUS")
+onready var  LowPassFilter:AudioEffectFilter
+export var max_freq = 22000
+export var min_freq = 200
+
+onready var player = get_tree().get_nodes_in_group("player")
+
 const music = [
 	'music1',
 	'music2'
 ]
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Background.play()
-	
 	connect("finished", self, "play_music")
 	play_music()
+	_on_Music_finished()
+
+
+func cut_freq():
+	LowPassFilter = AudioServer.get_bus_effect(mus, 0)
+	
+	if player.get_state() == "DyingState":
+		$FadeInOut.interpolate_property(LowPassFilter, "cutoff_hz", max_freq, min_freq, 2, 
+											Tween.TRANS_EXPO, Tween.EASE_OUT)
+		$FadeInOut.start()
+		print(LowPassFilter.get_cutoff())
+	else:
+		$FadeInOut.reset_all()
+		$FadeInOut.interpolate_property(LowPassFilter, "cutoff_hz", min_freq, max_freq.max_value, 2, 
+										Tween.TRANS_EXPO, Tween.EASE_OUT)
+		$FadeInOut.start()
 
 func play_music():
-	
+	randomize()
 	var rand_music = randi() % music.size()
 	var audiostream = load('res://Assets/Music/' + music[rand_music] + '.ogg')
-	set_stream(audiostream)
-	play()
+	audiostream.set_loop(false)
+	$Music.set_stream(audiostream)
+	$Music.set_volume_db(rand_range(0.2, 0.6))
+	print($Music.get_volume_db())
+	$Music.play()
+#	
 	
+
+#func _on_Timer_timeout():
+#	play_music()
+
+func _on_Music_finished():
+	play_music()
+
