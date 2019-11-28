8bd0b18364817fbe5c5b67543190b9263d101b48
Rafael <rafaelbordoni00@gmail.com>
2019-11-28 02:01:51 -0300

    Fix camera bug after respawning player

--- Levels/AuxiliaryTools/CameraLimiter.gd
+++ Levels/AuxiliaryTools/CameraLimiter.gd
@@ -20,14 +20,6 @@ func _on_CameraSetter_body_entered(body):
 			camera.drag_margin_left = 0.0
 			camera.drag_margin_right = 0.0
 
-#		if body.get_node_or_null("PlayerCamera") != null:
-#			var camera: Camera2D = body.get_node("PlayerCamera")
-#			body.remove_child(camera)
-#			camera_center.add_child(camera)
-#			camera.drag_margin_left = 0.0
-#			camera.drag_margin_right = 0.0
-#			camera.zoom = Vector2(0.5,0.5)
-
 func _on_CameraSetter_body_exited(body):
 	if body.is_in_group("player"):
 		var camera: Camera2D = get_tree().get_nodes_in_group("camera")[0] as PlayerCamera
@@ -36,14 +28,19 @@ func _on_CameraSetter_body_exited(body):
 				camera.followed_node = body
 				camera.drag_margin_left = 0.2
 				camera.drag_margin_right = 0.2
+			else:
+				var timer = Timer.new()
+				owner.add_child(timer)
+				timer.start(0.1)
+				yield(timer, "timeout")
+				for player in $Exit.get_overlapping_bodies():
+					if player.is_in_group("player"):
+						return
+				var player = get_tree().get_nodes_in_group("player")[0]
+				camera.followed_node = player
+				camera.drag_margin_left = 0.2
+				camera.drag_margin_right = 0.2
 
-#		if camera_center.get_node_or_null("PlayerCamera") != null:
-#			var camera: Camera2D = camera_center.get_node("PlayerCamera")
-#			camera_center.remove_child(camera)
-#			body.add_child(camera)
-#			camera.drag_margin_left = 0.2
-#			camera.drag_margin_right = 0.2
-#			camera.zoom = Vector2(0.5,0.5)
 
 
 
