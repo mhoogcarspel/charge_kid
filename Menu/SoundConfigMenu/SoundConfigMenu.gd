dd1be6cddec896bbab7f371ad2c63c45d47082e3
Ayrton <makkun1232@gmail.com>
2019-12-11 07:57:58 -0300

    Set buttons Neighbour

--- Menu/SoundConfigMenu/SoundConfigMenu.gd
+++ Menu/SoundConfigMenu/SoundConfigMenu.gd
@@ -1,6 +1,20 @@
 extends Control
 
-export(NodePath) var options_path
+export(ShortCut) var return_shortcut
+
+onready var main: Node
+onready var pause_menu: bool
 
 func _ready():
-		$CenterContainer/Options.get_children()[0].grab_focus()
+	if !get_tree().get_nodes_in_group("main").empty():
+		main = get_tree().get_nodes_in_group("main")[0]
+	$MarginContainer/Options/BGMSlider.grab_focus()
+
+func _on_Return_pressed():
+	if not pause_menu:
+		main.back_to_start()
+	else:
+		get_parent().pause_mode = PAUSE_MODE_PROCESS
+		get_parent().refocus()
+		get_parent().menu.get_node("Resume").shortcut = return_shortcut
+		self.queue_free()
