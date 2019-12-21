f9b2fd96ea0b461754f8351941b5efe30439a854
Rafael <rafaelbordoni00@gmail.com>
2019-12-21 20:15:28 -0300

    Add new shader effect

---
+++ Assets/ShaderEffects/RippleSource/RippleSource.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+export (float) var speed;
+export (float) var radius;
+export (float) var amplitude;
+export (float) var pulses;
+
+onready var shader = $Shader.get_material()
+
+func _process(delta):
+	shader.set_shader_param("speed", speed)
+	shader.set_shader_param("radius", radius)
+	shader.set_shader_param("amplitude", amplitude)
+	shader.set_shader_param("pulses", pulses)
+	shader.set_shader_param("scale", get_viewport().size*2)
+	$Shader.scale = get_viewport().size
