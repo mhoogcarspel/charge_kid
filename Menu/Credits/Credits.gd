e9213227f7b2bffa0d984271883e26363159ed8b
eldskald <rafaelbordoni00@gmail.com>
2020-03-13 11:44:51 -0300

    Update credits screen and menu navigation

--- Menu/Credits/Credits.gd
+++ Menu/Credits/Credits.gd
@@ -3,7 +3,14 @@ extends MarginContainer
 onready var main = get_tree().get_nodes_in_group("main")[0]
 
 func _ready():
-	$HBoxContainer/VBoxContainer/Back.grab_focus()
+	if get_tree().get_nodes_in_group("main").size() > 0:
+		main = get_tree().get_nodes_in_group("main")[0]
+	else:
+		main = null
 
-func _on_Back_pressed():
-	main.back_to_start()
+func _input(event):
+	if (event is InputEventKey or event is InputEventJoypadButton) and $Timer.is_stopped():
+		if main == null:
+			get_tree().quit()
+		else:
+			main.back_to_start()
