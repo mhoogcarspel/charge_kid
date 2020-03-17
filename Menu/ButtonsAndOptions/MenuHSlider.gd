75327608d2d7814766c97c28985d0330402d7d5e
Ayrton Makoto <makkun1232@gmail.com>
2020-03-17 18:05:35 -0300

    Somewhat Discrete Slider

--- Menu/ButtonsAndOptions/MenuHSlider.gd
+++ Menu/ButtonsAndOptions/MenuHSlider.gd
@@ -5,16 +5,17 @@ export(float) var slide_time
 
 onready var sound_control: SoundControl = get_tree().get_nodes_in_group("sound_control")[0]
 
+
 func _process(delta):
 	# Handling focus:
 	if has_focus():
 		var directional_input: Vector2 = main.control_handler.get_directional_input()
 		if $SliderTimer.is_stopped():
 			if directional_input.length() > 0:
-				$SliderTimer.start(slide_time)
 				if directional_input.x == 1:
 					$HSlider.value += $HSlider.step
 				elif directional_input.x == -1:
 					$HSlider.value -= $HSlider.step
-		if directional_input and not $SliderTimer.is_stopped():
+				$SliderTimer.start(slide_time)
+		if directional_input == Vector2.ZERO and not $SliderTimer.is_stopped():
 			$SliderTimer.stop()
