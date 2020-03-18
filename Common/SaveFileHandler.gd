d515d596da8c30d5dee1f733d2231b8f180e4dee
Ayrton Makoto <makkun1232@gmail.com>
2020-03-18 13:26:46 -0300

    Add a integrity checker for save files

--- Common/SaveFileHandler.gd
+++ Common/SaveFileHandler.gd
@@ -23,6 +23,13 @@ func load_progress() -> void:
 		file.open("save_progress.save", File.READ)
 		progress = parse_json(file.get_line())
 		file.close()
+		if !check_file_integrity():
+			file.open("save_progress_backup.save", File.WRITE)
+			file.store_line(to_json(progress))
+			file.close()
+			erase_progress()
+			print("ERROR: save file unreadable.")
+			print("A backup has been saved as save_progress_backup.save and the game progress reseted")
 
 func erase_progress() -> void:
 	var file = File.new()
@@ -31,4 +38,19 @@ func erase_progress() -> void:
 	progress["end"] = false
 	file.store_line(to_json(progress))
 	file.close()
+
+func check_file_integrity() -> bool:
+	if progress.keys().size() > 2:
+		print("ERROR: More itens than needed in save_progress.save")
+		return false
+	elif !("levels" in progress.keys()) or !("end" in progress.keys()):
+		print("ERROR: key missing in progress.save")
+		return false
+	elif !(progress["levels"] is float):
+		print("ERROR: levels value is not a number")
+		return false
+	elif !(progress["end"] is bool):
+		print("ERROR: levels value is not a int")
+		return false
+	return true
 	
