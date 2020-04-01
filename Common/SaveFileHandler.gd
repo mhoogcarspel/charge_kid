5a5f7dcbfa5e39ad7e23579bbb14d15bebc9bdab
Ayrton Makoto <makkun1232@gmail.com>
2020-04-01 14:19:32 -0300

    More checks on save file

--- Common/SaveFileHandler.gd
+++ Common/SaveFileHandler.gd
@@ -21,15 +21,14 @@ func load_progress() -> void:
 	var file = File.new()
 	if file.file_exists("save_progress.save"):
 		file.open("save_progress.save", File.READ)
-		progress = parse_json(file.get_line())
-		file.close()
-		if !check_file_integrity():
+		var file_string: String = file.get_line()
+		if !check_file_integrity(file_string):
 			file.open("save_progress_backup.save", File.WRITE)
-			file.store_line(to_json(progress))
-			file.close()
+			file.store_line(file_string)
 			erase_progress()
 			print("ERROR: save file unreadable.")
 			print("A backup has been saved as save_progress_backup.save and the game progress reseted")
+		file.close()
 
 func erase_progress() -> void:
 	var file = File.new()
@@ -39,7 +38,15 @@ func erase_progress() -> void:
 	file.store_line(to_json(progress))
 	file.close()
 
-func check_file_integrity() -> bool:
+func check_file_integrity(file_string: String) -> bool:
+	var error_message = validate_json(file_string)
+	
+	if error_message:
+		print(error_message)
+		print("ERROR: Save file is not on Json format")
+		return false
+	progress = parse_json(file_string)
+	
 	if progress.keys().size() > 2:
 		print("ERROR: More itens than needed in save_progress.save")
 		return false
