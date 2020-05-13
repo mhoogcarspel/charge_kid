cb9fc206ff3fefd4fffe5d3a0969ed7442038098
eldskald <rafaelbordoni00@gmail.com>
2020-05-13 20:17:08 -0300

    Update energy gate sfx volume and 15th level message

--- source/levels/base-nodes/messages/lv15-unique.gd
+++ source/levels/base-nodes/messages/lv15-unique.gd
@@ -6,7 +6,7 @@ export (float) var message_time = 2.0
 
 
 func _on_Lv15Unique_body_entered(body):
-	if body.is_in_group("player") and $Timer.is_stopped():
+	if body.is_in_group("player") and not $Timer.is_stopped():
 		get_parent().write(message, message_time)
 
 func _on_SubArea_body_exited(body):
