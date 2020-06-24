671b6a79abaf1edafe81f200a724582b47881023
Ayrton Makoto <makkun1232@gmail.com>
2020-06-23 21:16:22 -0300

    ADD Progress achievement to last_finish_line

--- source/levels/base-nodes/finish-line/last-finish-line.gd
+++ source/levels/base-nodes/finish-line/last-finish-line.gd
@@ -6,6 +6,10 @@ export (PackedScene) var speedrun_finish
 func _on_FinishLine_body_entered(body):
 	if body.is_in_group("player"):
 		if get_tree().get_nodes_in_group("main").size() > 0:
+			var level:int = get_tree().get_nodes_in_group("main")[0].get_nodes_in_group("level")[0].level
+			AchievementsAndStatsObserver.set_stat("main_levels_finished", level)
+			if level == 10:
+				AchievementsAndStatsObserver.indicate_achievement_progress("finish_main_game", level, 17)
 			var main = get_tree().get_nodes_in_group("main")[0]
 			var speedrun_mode = main.get_node("SpeedrunMode")
 			if speedrun_mode.is_active() and speedrun_mode.category == "times":
