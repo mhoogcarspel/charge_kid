2541877c287660cb6349a8ba5f0d8b7ac2543eba
Ayrton Makoto <makkun1232@gmail.com>
2020-06-23 21:06:11 -0300

    ADD game progress achievements

--- source/levels/base-nodes/finish-line/finish-line.gd
+++ source/levels/base-nodes/finish-line/finish-line.gd
@@ -6,6 +6,11 @@ func _on_FinishLine_body_entered(body):
 	if body.is_in_group("player") and get_tree().get_nodes_in_group("main").size() > 0:
 		var main = get_tree().get_nodes_in_group("main")[0]
 		var level = get_tree().get_nodes_in_group("level")[0].level
+		AchievementsAndStatsObserver.set_stat("main_levels_finished", level)
+		
+		if level == 10:
+			AchievementsAndStatsObserver.indicate_achievement_progress("finish_main_game", level, 17)
+			
 		var next_level = main.get_node("SaveFileHandler").levels[level]
 		main.change_scene(next_level)
 
