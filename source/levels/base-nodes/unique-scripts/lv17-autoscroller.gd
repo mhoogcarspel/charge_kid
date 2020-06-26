489c8097ad0de6de6f736d33fc64f9ef919ad1b9
vicentereis <vicentersfa@gmail.com>
2020-06-26 00:23:52 -0300

    shake

--- source/levels/base-nodes/unique-scripts/lv17-autoscroller.gd
+++ source/levels/base-nodes/unique-scripts/lv17-autoscroller.gd
@@ -34,8 +34,10 @@ func _physics_process(delta):
 			else:
 				if not end_of_level:
 					camera.shake_screen(30, 2)
+					$SFX.play()
 					self.speed *= 3
 					end_of_level = true
+					
 				$EnergyGate.position.x += speed*delta
 
 
@@ -56,12 +58,14 @@ func _on_Tween_tween_completed(object, key):
 	if object == self:
 		$PauseTimer.start(2)
 		camera.shake_screen(30, 3)
+		$SFX.play()
 	elif object == top_gate and checkpoint_3_event_part == 1:
 		checkpoint_3_event_part = 2
 		$Tween.interpolate_property(top_gate, "position", null, Vector2(2248, 360), 6.0,
 										Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$Tween.start()
 		camera.shake_screen(24)
+		$SFX.play()
 	elif object == top_gate and checkpoint_3_event_part == 2:
 		checkpoint_3_event_part = 3
 		$PauseTimer.start(3)
@@ -71,6 +75,7 @@ func _on_Tween_tween_completed(object, key):
 										Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$Tween.start()
 		camera.shake_screen(24)
+		$SFX.play()
 	elif object == bottom_gate and checkpoint_3_event_part == 4:
 		checkpoint_3_event_part = 5
 		$PauseTimer.start(0.5)
@@ -82,6 +87,7 @@ func _on_PauseTimer_timeout():
 	match checkpoint_3_event_part:
 		0:
 			camera.shake_screen(30, 3)
+			$SFX.play()
 		1:
 			$Tween.interpolate_property(top_gate, "position", null, Vector2(2248, 56), 2.0,
 										Tween.TRANS_LINEAR, Tween.EASE_IN)
@@ -98,5 +104,6 @@ func _on_PauseTimer_timeout():
 			active = true
 		6:
 			camera.shake_screen(30, 3)
+			$SFX.play()
 
 
