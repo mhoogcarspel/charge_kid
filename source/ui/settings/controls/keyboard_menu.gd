dcb9a4109f4dae881bd0837ba3d99ab354eb0dfc
Ayrton Makoto Euzebio sato <makkun1232@gmail.com>
2020-09-08 17:42:41 -0300

    Making a Keyboard especial menu

--- source/ui/settings/controls/keyboard_menu.gd
+++ source/ui/settings/controls/keyboard_menu.gd
@@ -18,7 +18,7 @@ func _ready():
 	for key in control_handler.actions_dictionary.keys():
 		var button = button_model.instance()
 		map.add_child(button)
-		button.get_node("Button").parse(self, key, control_handler.actions_dictionary[key], control_handler, type)
+		button.get_node("Button").parse(self, key, control_handler.actions_dictionary[key], control_handler, "Keyboard")
 		
 		##################### Setting Buttons Neighbours ############################
 		if (map.get_children().size() > 1):
@@ -33,12 +33,8 @@ func _ready():
 	defaults_button.focus_neighbour_top = map.get_children()[-1].get_node("Button").get_path()
 	map.get_children()[-1].get_node("Button").focus_neighbour_bottom = defaults_button.get_path()
 	#############################################################################################################
-#	match type:
-#		"Keyboard":
-#			change_button.text = "Controller Bindings"
-#		"Controller":
-#			change_button.text = "Keyboard Bindings"
 	
-	add_remove_model_buttton()
+	defaults_button.focus_neighbour_bottom = change_button.get_path()
+	change_button.focus_neighbour_top = defaults_button.get_path()
 	defaults_button.connect("pressed", self, "_on_Defaults_pressed")
 	map.get_children()[0].get_node("Button").grab_focus()
