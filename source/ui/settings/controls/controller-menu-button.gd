b65c1e9779efcac8c9cecc883febe288c0c01d4f
Ayrton Makoto Euzebio sato <makkun1232@gmail.com>
2020-09-09 10:49:57 -0300

    Controller Menu work on settings menu

--- source/ui/settings/controls/controller-menu-button.gd
+++ source/ui/settings/controls/controller-menu-button.gd
@@ -1,14 +1,49 @@
 extends ButtonModel
+class_name ControllerSettings
 
 export(int) var device
-export(int) var index
+export(String, "DPAD Up", "DPAD Down", "DPAD Left", "DPAD Right", "Face Button Bottom", 
+				"Face Button Right", "Face Button Left", "Face Button Top", "L", "R", "L2", "R2", 
+				"L3", "R3", "Start", "Select") var button
 
 onready var button_getter: ButtonGetter = main.control_handler
+onready var gamepad_index = {
+	"DPAD Up": 12,
+	"DPAD Down": 13,
+	"DPAD Left": 14,
+	"DPAD Right": 15,
+	
+	"Face Button Bottom": 0,
+	"Face Button Right": 1,
+	"Face Button Left": 2,
+	"Face Button Top": 3,
+	
+	"L": 4,
+	"R": 5,
+	"L2": 6,
+	"R2": 7,
+	
+	"L3": 8,
+	"R3": 9,
+	
+	"Start": 11,
+	"Select": 10
+	}
 
-var input_event: InputEventJoypadButton
 var action: String
+var input_event: InputEventJoypadButton
 
 func _ready():
 	input_event = InputEventJoypadButton.new()
-	input_event.button_index = index
+	input_event.button_index = gamepad_index[button]
 	input_event.device = device
+	
+	for command in button_getter.actions_list:
+		if button_getter.key_in_list(input_event, button_getter.get_type_button_list(command, InputEventJoypadButton)):
+			if !command.begins_with("ui_"):
+				action = main.actions[command]
+				break
+		action = "None"
+
+func _process(delta):
+	self.text = action
