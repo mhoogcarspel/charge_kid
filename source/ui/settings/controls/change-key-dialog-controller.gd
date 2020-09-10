b5e3ac65b2fe30e94ef8be79d8b9687416b8bf7c
Ayrton <makkun1232@gmail.com>
2020-09-10 11:19:15 -0300

    Button from Controller Settings now spawns a dialog box

--- source/ui/settings/controls/change-key-dialog-controller.gd
+++ source/ui/settings/controls/change-key-dialog-controller.gd
@@ -1,6 +1,8 @@
 extends Control
 
-onready var main = get_tree().get_nodes_in_group("main")
+export(PackedScene) var button_model
+
+onready var main = get_tree().get_nodes_in_group("main")[0]
 onready var parent: Node
 onready var key: InputEventJoypadButton
 onready var key_name: String
@@ -12,7 +14,8 @@ func _ready():
 	parent.pause_mode = PAUSE_MODE_STOP
 	self.pause_mode = PAUSE_MODE_PROCESS
 	for command in main.actions.keys():
-		if command.begin_with("action_"):
+		if command.begins_with("action_"):
 			command_list.append(command)
 		pass
-	$Title.text = "Choose a action for " + key_name
+	print(command_list)
+	$CenterContainer/MarginContainer/CenterContainer/VBoxContainer/Label.text = "Choose a action for " + key_name
--- source/ui/settings/controls/controller-menu.gd
+++ source/ui/settings/controls/controller-menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
+export(PackedScene) var dialog_box
+
 onready var main = get_tree().get_nodes_in_group("main")[0]
 onready var pause_menu : bool = get_parent().pause_menu
 onready var settings_menu: Node
 
 func _ready():
+	parse_info()
 	$Center/Margin/Menu/Menu/Scheme/LeftList.get_children()[0].grab_focus()
 
 
@@ -14,6 +17,23 @@ func _on_ChangeLayout_pressed():
 func _on_Return_pressed():
 	settings_menu.quit()
 
+func open_dialog_box(key: InputEventJoypadButton, key_name: String,
+						button_getter: ButtonGetter = main.control_handler) -> void:
+	var dialog_box_instance = dialog_box.instance()
+	dialog_box_instance.parent = self
+	dialog_box_instance.key = key
+	dialog_box_instance.key_name = key_name
+	self.add_child(dialog_box_instance)
 
 func _on_Defaults_pressed():
 	settings_menu.load_defaults()
+
+func parse_info() -> void:
+	var left_buttons: Array = $Center/Margin/Menu/Menu/Scheme/LeftList.get_children()
+	var right_buttons: Array = $Center/Margin/Menu/Menu/Scheme/RightList.get_children()
+	for button in left_buttons:
+		if button is ControllerSettingsButton:
+			button.menu = self
+	for button in right_buttons:
+		if button is ControllerSettingsButton:
+			button.menu = self
