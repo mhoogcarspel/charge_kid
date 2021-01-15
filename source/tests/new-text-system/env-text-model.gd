ac3aa71d1ec4eb30c88bcd72b84fe86afaf6653a
eldskald <rafaelbordoni00@gmail.com>
2021-01-15 18:14:26 -0300

    Remove unused files

--- source/tests/new-text-system/env-text-model.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-func _ready():
-	self.set("modulate", Color(1,1,1,0))
-	$Tween.interpolate_property(self, "modulate", Color(1,1,1,0), Color(1,1,1,1), 2.0,Tween.TRANS_LINEAR, Tween.EASE_IN)
-	$Tween.start()
